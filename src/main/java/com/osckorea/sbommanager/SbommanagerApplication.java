6ca72b82ca920813d6182efcbedfd9e4ffae2ba3
unknown <gene.lee@osckorea.com>
2024-12-23 17:49:28 +0900

    Initial commit

---
+++ src/main/java/com/osckorea/sbommanager/SbommanagerApplication.java
@@ -0,0 +1,13 @@
+package com.osckorea.sbommanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbommanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbommanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osckorea/sbommanager/SbommanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osckorea.sbommanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbommanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
