4a436100deedc07587881cfc3754c778e0413570
dorongrong <tjdgns431@naver.com>
2024-12-27 03:22:43 +0900

    swagger, feign-client Add

--- src/main/java/com/osckorea/sbommanager/controller/TestController.java
+++ src/main/java/com/osckorea/sbommanager/controller/TestController.java
@@ -2,6 +2,8 @@ package com.osckorea.sbommanager.controller;
 
 import com.osckorea.sbommanager.model.User;
 import com.osckorea.sbommanager.service.TestService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,41 +15,48 @@ import java.util.List;
 @RestController
 @RequestMapping("/sample-api/v1/test")
 @RequiredArgsConstructor
+@Tag(name = "Sample API", description = "템플릿 샘플 API 그룹")
 @Slf4j
 public class TestController {
     //    private final CommandBoardService commandService;
     private final TestService testService;
 
+    @Operation(summary = "Server Test", description = "서버를 테스트합니다.")
     @GetMapping()
     public String test01() {
         return "hello";
     }
 
     //직접 생성한 쿼리용
+    @Operation(summary = "Get All User", description = "직접 생성한 쿼리로 User를 가져옵니다.")
     @GetMapping("/user02")
     public ResponseEntity<List<User>> getAllUser() {
         List<User> users  = testService.getAllUsers();
         return ResponseEntity.ok(users);
     }
 
+    @Operation(summary = "Get All User", description = "모든 User를 가져옵니다.")
     @GetMapping("/user")
     public ResponseEntity<Iterable<User>> getAllUser02() {
         Iterable<User> users  = testService.getAllUsers02();
         return ResponseEntity.ok(users);
     }
 
+    @Operation(summary = "User Create", description = "User를 생성합니다.")
     @PostMapping("/user")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User createdUser = testService.createUser(user);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "User Edit", description = "User를 수정합니다.")
     @PutMapping("/user/{id}")
     public ResponseEntity<User> editUser(@PathVariable("id") Long id, @RequestBody User user) {
         User createdUser = testService.editUser(id, user);
         return new ResponseEntity<>(createdUser, HttpStatus.OK);
     }
 
+    @Operation(summary = "User Delete", description = "User를 삭제합니다.")
     @DeleteMapping("/user/{id}")
     public ResponseEntity<Void> deleteUserById(@PathVariable("id") Long id) {
         testService.deleteUser(id);
